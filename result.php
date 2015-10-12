56cbb0da377546897f56192826c52ba61e100fbd
jeykboy <jeyk.joseph@WKS-126.corp.bmielite.com>
2015-10-12 15:32:40 -0400

    Progress 3
    4 Hours of work

--- result.php
+++ result.php
@@ -6,7 +6,11 @@
 
 <style>
 
-#resultBox { width:400px; height:800; margin:0 auto;}
+html { font-family:monospace ,Consolas, "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", Monaco, "Courier New"}
+
+#resultBox { width:500px; height:700; margin:0 auto;}
+.results { width:500px; min-height:700px;}
+.target { width:500px; height:50px; margin:0 auto; text-align:left; margin-top:50px;}
 
 
 </style>
@@ -51,7 +55,7 @@ $myCode = $_POST['code'];
 //Converts Code and HTML Characters to String
 $spitCode = htmlspecialchars($myCode);
 
-$spitCode = strtolower($spitCode);
+
 
 
 /*
@@ -72,15 +76,45 @@ echo substr_count($spitCode, "meta"); // will echo Times Meta is in code
 */
 
 
-echo $spitCode;
+
+
 
 ?>
+
+<textarea class="results" id="to_copy" name="results"> <?php echo $spitCode; ?> </textarea>
+<button id='copy_button' data-clipboard-target='to_copy' onClick="add_content()" value="write"><b>Copy To Clipboard</b></button>
+
 
 
 </div>
 
+<div id="target" class="target"></div>
 
 
+
+<!------ START OF COPY TO CLIPBOARD------>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="javascripts/ZeroClipboard.js"></script>
+    
+    <script language="JavaScript">
+$(document).ready(function() {
+
+  var clip = new ZeroClipboard($("#copy_button"), {
+      moviePath: "javascripts/ZeroClipboard.swf"
+    });     
+});
+</script>
+
+<script type="text/javascript">
+function add_content(){
+	document.getElementById("target").innerHTML="Copied";
+
+}
+
+</SCRIPT>
+
+<!------ END OF COPY TO CLIPBOARD------>
 
 </body>
 </html>
