25fbfa75372459486a35e9d87440ecaf0aad39e8
jeykboy <jeyk.joseph@WKS-126.corp.bmielite.com>
2015-10-12 16:28:43 -0400

    Started UX
    Added Bootstrap

--- production.php
+++ production.php
@@ -4,6 +4,16 @@
 <meta charset="utf-8">
 <title>Production</title>
 
+<!-- Boootstrap Start -->
+
+
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+
+<!-- Boootstrap End -->
+
 <style>
 #code { width:400px; min-height:500px;}
 
--- result.php
+++ result.php
@@ -4,14 +4,27 @@
 <meta charset="utf-8">
 <title>Code</title>
 
+
+
+<!-- Boootstrap Start -->
+
+
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+
+<!-- Boootstrap End -->
 <style>
 
 html { font-family:monospace ,Consolas, "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", Monaco, "Courier New"}
 
-#resultBox { width:500px; height:700; margin:0 auto;}
-.results { width:500px; min-height:700px;}
-.target { width:500px; height:50px; margin:0 auto; text-align:left; margin-top:50px;}
+body { background-color:#EEEEEE;}
 
+#resultBox { width:500px; height:auto; margin:0 auto;}
+.results { width:500px; min-height:500px; color:#FF7321; background-color:#252525;}
+.target { width:500px; height:50px; margin:0 auto; text-align:left; margin-top:30px; color:#5cb85c; font-weight:bold;}
+#jump { width:400px; height:250px; margin:0 auto;}
 
 </style>
 
@@ -22,6 +35,14 @@ html { font-family:monospace ,Consolas, "Andale Mono", "Lucida Console", "Lucida
 <body>
 
 
+
+<div id="jump">
+<div class="jumbotron">
+  <h1>E-mail</h1>
+  <p>This is where the code stops</p>
+  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+</div>
+</div>
 <?php
 
 
@@ -36,9 +57,7 @@ $myCode = $_POST['code'];
 
 
 <!---- LOOK UP HTML BUTTONS ---->
-<a href="http://localhost/practice/rss/production.php"><button>Return</button></a>
-
-
+<a href="http://localhost/practice/rss/production.php"><span class="glyphicon glyphicon-refresh" aria-hidden="true"></span></a>
 <br>
 <br>
 
@@ -82,7 +101,7 @@ echo substr_count($spitCode, "meta"); // will echo Times Meta is in code
 ?>
 
 <textarea class="results" id="to_copy" name="results"> <?php echo $spitCode; ?> </textarea>
-<button id='copy_button' data-clipboard-target='to_copy' onClick="add_content()" value="write"><b>Copy To Clipboard</b></button>
+<button class="btn" id='copy_button' data-clipboard-target='to_copy' onClick="add_content()" value="write"><b>Copy To Clipboard</b></button>
 
 
 
